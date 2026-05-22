91939abda27d26f07a232cf39fef489e561e3ba8
Nguyễn Việt Dũng <vietdung0322@gmail.com>
2026-05-22 22:55:00 +0700

    Delete PBLtest directory

--- PBLtest/my_string.c
+++
@@ -1,9 +0,0 @@
-#include "my_string.h"
-
-int mystring_len(const char *str) {
-    int len = 0;
-    while (str[len] != '\0') {
-        len++;
-    }
-    return len;
-}
--- PBLtest/my_string.h
+++
@@ -1,4 +0,0 @@
-#ifndef MY_STRING_H
-#define MY_STRING_H
-int mystring_len(const char *str);
-#endif 
